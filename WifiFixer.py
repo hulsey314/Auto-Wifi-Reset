ccc45ad1d5a7592d76eea0da006433b4884a9fd2
hulsey314 <hulsey314@gmail.com>
2017-03-08 19:11:25 -0600

    Update WifiFixer.py

--- WifiFixer.py
+++ WifiFixer.py
@@ -1,5 +1,5 @@
 # WifiFixer v.1.0.0
-# Monitors Wi-Fi to check if internet is connected. Resets Wifi adapter
+# Monitors Wi-Fi to check if internet is connected. Resets Wi-fi adapter
 # when no connection is found
 # Windows only.  Must be run as admin
 
